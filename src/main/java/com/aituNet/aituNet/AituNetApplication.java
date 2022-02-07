20f71c7016b385a65f0095f1af16a76a7107a97f
kanatbkeeep <clashofkanat@gmail.com>
2022-02-07 19:09:32 +0600

    Initial commit

---
+++ src/main/java/com/aituNet/aituNet/AituNetApplication.java
@@ -0,0 +1,13 @@
+package com.aituNet.aituNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AituNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AituNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aituNet/aituNet/AituNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aituNet.aituNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AituNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
