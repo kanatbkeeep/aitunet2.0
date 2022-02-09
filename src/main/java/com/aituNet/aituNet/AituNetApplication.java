950b240dae57ee16f15eec3851604eaf356639b4
elhanz <201338@astanait.edu.kz>
2022-02-09 19:49:37 +0600

    asd

--- src/main/java/com/aituNet/aituNet/AituNetApplication.java
+++ src/main/java/com/aituNet/aituNet/AituNetApplication.java
@@ -1,13 +1,28 @@
 package com.aituNet.aituNet;
 
+
+import com.aituNet.aituNet.entities.User;
+import com.aituNet.aituNet.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class AituNetApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AituNetApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AituNetApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveUser(new User("asd", "asd", "sd", new ArrayList<>(), new ArrayList<>()));
+			userService.addRoleToUser("asd", "ROLE_USER");
+        };
+    }
 }
--- src/main/java/com/aituNet/aituNet/repo/PostRepo.java
+++ src/main/java/com/aituNet/aituNet/repo/PostRepo.java
@@ -4,5 +4,5 @@ import com.aituNet.aituNet.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepo extends JpaRepository<Post, Long> {
-
+    Post findByAuthor(String name);
 }
