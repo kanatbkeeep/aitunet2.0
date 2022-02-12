76e44400f0f09538999b4e09f8af9fe9862026f1
ko1z-se-2015 <201402@astanait.edu.kz>
2022-02-12 23:26:40 +0600

    friends and requests

--- src/main/java/com/aituNet/aituNet/controllers/ApplicationController.java
+++ src/main/java/com/aituNet/aituNet/controllers/ApplicationController.java
@@ -1,12 +1,34 @@
 package com.aituNet.aituNet.controllers;
 
 
+import com.aituNet.aituNet.entities.Application;
+import com.aituNet.aituNet.request.CancelApplication;
+import com.aituNet.aituNet.request.SendToRequest;
+import com.aituNet.aituNet.service.ApplicationServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/requests")
 public class ApplicationController {
+
+    private final ApplicationServiceImpl applicationService;
+
+    @PostMapping("/send")
+        public ResponseEntity SendRequest (@RequestBody Application application){
+        applicationService.sendApplication(application);
+            return new ResponseEntity("request created", HttpStatus.CREATED);
+        }
+        @PostMapping("/cancel")
+    public  ResponseEntity CancelRequest(@RequestBody CancelApplication cancelApplication){
+        applicationService.deleteApplication(cancelApplication.getRequest_id());
+            return new ResponseEntity("request is deleted", HttpStatus.CREATED);
+        }
+        @GetMapping("/show")
+    public ResponseEntity ShowRequest(@RequestBody SendToRequest sendToRequest){
+            return ResponseEntity.ok().body(applicationService.showApplication(sendToRequest.getSendToId()));
+        }
 }
--- src/main/java/com/aituNet/aituNet/controllers/FriendsController.java
+++ src/main/java/com/aituNet/aituNet/controllers/FriendsController.java
@@ -1,12 +1,34 @@
 package com.aituNet.aituNet.controllers;
 
 
+import com.aituNet.aituNet.entities.Friends;
+import com.aituNet.aituNet.request.DeleteFriendRequest;
+import com.aituNet.aituNet.request.MyIdForFriendsRequest;
+import com.aituNet.aituNet.service.FriendServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/friends")
 public class FriendsController {
+
+    private final FriendServiceImpl friendService;
+
+    @PostMapping("/add")
+    public ResponseEntity AddFrined(@RequestBody Friends friends){
+        friendService.addFriend(friends);
+        return new ResponseEntity("friend created", HttpStatus.CREATED);
+    }
+    @PostMapping("/delete")
+    public ResponseEntity DeleteFriend(@RequestBody DeleteFriendRequest deleteFriendRequest){
+        friendService.deleteFriend(deleteFriendRequest.getFriendId());
+        return new ResponseEntity("friend is deleted", HttpStatus.CREATED);
+    }
+    @GetMapping("/show")
+    public ResponseEntity ShowFriends(@RequestBody MyIdForFriendsRequest myIdForFriendsRequest){
+        return ResponseEntity.ok().body(friendService.showFriends(myIdForFriendsRequest.getMyId()));
+    }
 }
